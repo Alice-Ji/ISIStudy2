7a0f664d30d226157d63cff9bb50b3ae4de28aa1
Alice Ji <rji3@illinois.edu>
2025-08-21 13:44:05 -0500

    Update script.js

--- auv/script.js
+++ auv/script.js
@@ -172,8 +172,11 @@ function renderFeed() {
       ${mediaContent}
       <p>${post.caption}</p>
       <div class="post-actions">
-          <img id="like-btn-${index}" src="https://raw.githubusercontent.com/ruochongji/affordancePSIPSR/main/ins-like1.png" 
-               alt="Like" class="action-icon" onclick="window.likePost(${index})">
+          <img id="like-btn-${index}" 
+     src="${post.liked 
+       ? "https://raw.githubusercontent.com/ruochongji/affordancePSIPSR/main/ins-like2.png" 
+       : "https://raw.githubusercontent.com/ruochongji/affordancePSIPSR/main/ins-like1.png"}" 
+     alt="Like" class="action-icon" onclick="window.likePost(${index})">
           <img id="comment-btn-${index}" src="https://raw.githubusercontent.com/ruochongji/affordancePSIPSR/main/ins-comment.png"
                alt="Comment" class="action-icon" onclick="window.toggleComment(${index})">
       </div>
